dd3fbed7c1f76708472d258824e0ace73b5815ff
JonathanJCollins <jcollins15335@att.net>
2014-09-23 15:08:08 -0500

    Updatedmain
    Updatedmain

--- ConsoleApplication1/ConsoleApplication1/SeatingChart.cpp
+++ ConsoleApplication1/ConsoleApplication1/SeatingChart.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 void printSeats(char seats[15][30]){
-	
+	cout << "\t123456789012345678901234567890" << endl;
+	cout << endl;
 	for (int i = 0; i < 15; ++i){
 		cout << "Row " << i+1 << "\t";
 		for (int j = 0; j < 30; ++j){
@@ -12,6 +13,7 @@ void printSeats(char seats[15][30]){
 		}
 		cout << endl;
 	}
+	cout << endl;
 }
 
 int main(){
@@ -21,6 +23,48 @@ int main(){
 			seats[i][j] = '#';
 		}
 	}
+	int yesNo = 0;
+	cout << "Would you like to import seat prices? 1 for yes, anything else for no.";
+	cin >> yesNo;
+
+	if (yesNo == 1) {
+		// IMPORT FUNCTION HERE!
+	}
+	else {
+		// MANUAL SET PRICE FUNCTION
+	}
+
+	int selection = 0;
+	while (selection != -1){
+		cout << "What would you like to do?" << endl 
+			<< "1 : List Seating Chart" << endl 
+			<< "2 : Show sales info" << endl 
+			<< "3 : Purchase Seats" << endl 
+			<< "4 : Save Current Seat Prices to File" << endl 
+			<< "-1: QUIT" << endl;
+
+		cin >> selection;
+		
+		if (selection == 1) {
+			cout << "Current Available Seats: " << endl << endl;
+			printSeats(seats);
+		}
+		else if (selection == 2) {
+			// STATISTICS CHART
+		}
+		else if (selection == 3) {
+			// SEAT SELLING 
+		}
+		else if (selection == 4){
+			// SAVE PRICES TO TEXT FILE
+		}
+		else {
+			cout << "INVALID SELECTION" << endl << endl;
+		}
+	}
+
 	printSeats(seats);
+
+	//PRINT STATISTICS DOWN HERE AS WELL.
 	return 0;
 }
